131bd5cd4998b761f06adc3a7f569faa958fa985
BADMOODXX <90480113+BADMOODXX@users.noreply.github.com>
2021-09-10 22:54:32 +0200

    Add files via upload

---
+++ JournalApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+namespace Learning
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Journal
+			Journal j = new Journal(1234);
+			while (j.IsRunning)
+			{
+				j.Menu();
+				char token = Console.ReadLine()[0];
+				switch (token)
+				{
+					case '1':
+						j.Unlock();
+						break;
+					case '2':
+						j.CreateEntry();
+						break;
+					case '3':
+						j.ReadEntry();
+						break;
+					case '4':
+						j.Lock();
+						break;
+					case '5':
+						j.Quit();
+						break;
+					default:
+						Console.WriteLine("Invalid token");
+						break;
+				}
+
+			}
+			
+		}
+	}
+}
